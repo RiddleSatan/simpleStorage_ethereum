52012408ddef052256da5787688004784ca41087
RiddleSatan <mynameisompandey007@gmail.com>
2024-08-22 10:41:11 +0530

    storageFactory

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -2,12 +2,25 @@
 
 pragma solidity >=0.8.26;
 
-import {simpleStorage} from "./simpleStorage.sol";
+import { simpleStorage } from "./simpleStorage.sol";
 
 contract StorageFactory {
-    simpleStorage public mySimpleStorage; //the reason why we use simpleStorage to create its instace is because the new simpleStorage() when deployed returns the address of the contract But if u use address datatype to create the mySimpleStorage instead then in thar case you need to handle te address according to that as the variable created by the address key word are use to store the etherium address rather than contract adress
+    // simpleStorage public mySimpleStorage; //the reason why we use simpleStorage to create its instace is because the new simpleStorage() when deployed returns the address of the contract But if u use address datatype to create the mySimpleStorage instead then in thar case you need to handle te address according to that as the variable created by the address key word are use to store the etherium address rather than contract adress
+
+    simpleStorage[] public listOfSimpleStorageContracts;
 
     function createSimpleStorageContract() public {
-        mySimpleStorage = new simpleStorage();
+        simpleStorage newSimpleStorageContracts = new simpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContracts);      
     }
+    
+    function sfStore(uint256 _index,uint256 _number) public {
+                simpleStorage mySimpleStorage=listOfSimpleStorageContracts[_index];
+                mySimpleStorage.store(_number);
+    }
+
+    function sfGet(uint256 _index) public view returns(uint256){
+simpleStorage mySimpleStorage = listOfSimpleStorageContracts[_index];
+return mySimpleStorage.getVal();
+}
 }
